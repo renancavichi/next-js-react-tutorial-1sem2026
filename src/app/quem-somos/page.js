6ed0ce36be9fc7e4b1244f1566c017fb483fa6a2
Renan Cavichi <renancavichi@gmail.com>
2026-02-23 22:19:09 -0300

    aula 1 - primeiros passos no next.js

--- src/app/quem-somos/page.js
+++ src/app/quem-somos/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
-export default function Home() {
+export default function QuemSomos() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>Olá Mundo Next.js e React!</h1>
+          <h1>Quem Somos</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
